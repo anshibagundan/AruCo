8b005f28d874cca57b1f5421b680f7b012d5f4b3
usamaru33 <raioraioraio15@gmail.com>
2024-07-02 18:37:11 +0900

    ChangeQuizScene直しました

--- dementia_quiz_VR/Assets/C#/ChageQuizScene.cs
+++ dementia_quiz_VR/Assets/C#/ChageQuizScene.cs
@@ -3,13 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class ChangeQuizScene : MonoBehaviour
 {
+    private bool IsChanged = false;
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("OnTriggerEnter called"); // ?f?o?b?O???O???
-        if (other.gameObject.CompareTag("QuizCollider"))
+        if (other.gameObject.CompareTag("QuizCollider") && IsChanged == false)
         {
             Debug.Log("QuizCollider detected"); // ?f?o?b?O???O???
             SceneManager.LoadScene("QuizScene");
+            IsChanged = true;
         }
     }
 }
