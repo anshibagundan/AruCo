76bd5e09b8610f2582e96c5c211b2dfa20baa733
usamaru33 <raioraioraio15@gmail.com>
2024-07-02 19:33:32 +0900

    コライダーの問題解決しました

--- dementia_quiz_VR/Assets/C#/ChageQuizScene.cs
+++ dementia_quiz_VR/Assets/C#/ChageQuizScene.cs
@@ -3,16 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class ChangeQuizScene : MonoBehaviour
 {
-    private bool IsChanged = false;
+    private bool IsChanged_1st = false;
+    private bool IsChanged_2nd = false;
+    private bool IsChanged_3rd = false;
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("OnTriggerEnter called"); // ?f?o?b?O???O???
-        if (other.gameObject.CompareTag("QuizCollider") && IsChanged == false)
+        if (other.gameObject.CompareTag("QuizCollider_1st") && IsChanged_1st == false)
         {
-            Debug.Log("QuizCollider detected"); // ?f?o?b?O???O???
+            Debug.Log("QuizCollider detected"); //
             SceneManager.LoadScene("QuizScene");
-            IsChanged = true;
+            IsChanged_1st = true;
+        }
+        else if (other.gameObject.CompareTag("QuizCollider_2nd") && IsChanged_2nd == false)
+        {
+            Debug.Log("QuizCollider detected"); //
+            SceneManager.LoadScene("QuizScene");
+            IsChanged_2nd = true;
+        }
+        else if (other.gameObject.CompareTag("QuizCollider_3rd") && IsChanged_3rd == false)
+        {
+            Debug.Log("QuizCollider detected"); //
+            SceneManager.LoadScene("QuizScene");
+            IsChanged_3rd = true;
         }
     }
 }
