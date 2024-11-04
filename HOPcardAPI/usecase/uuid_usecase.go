c3961ce0cd160efe73190ec2563e7af503773ce1
tarakokko3233 <tanahashist1230@outlook.jp>
2024-11-04 13:57:03 +0900

    サーバーで一意のCodeとUUIDを生成するように変更

--- HOPcardAPI/usecase/uuid_usecase.go
+++ HOPcardAPI/usecase/uuid_usecase.go
@@ -4,10 +4,11 @@ import (
 	"HOPcardAPI/domain/models"
 	"HOPcardAPI/domain/repositories"
 	"HOPcardAPI/domain/services"
+	"math/rand"
 )
 
 type UUIDUseCase interface {
-	CreateUUID(code int) (*models.UUID, error)
+	CreateUUID() (*models.UUID, error)
 	GetUUIDByCode(code int) (*models.UUID, error)
 }
 
@@ -23,7 +24,25 @@ func NewUUIDUseCase(repo repositories.UUIDRepository, service services.UUIDServi
 	}
 }
 
-func (uc *uuidUseCase) CreateUUID(code int) (*models.UUID, error) {
+func (uc *uuidUseCase) CreateUUID() (*models.UUID, error) {
+	var code int
+
+	// 一意な6桁のコードが生成されるまでループ
+	for {
+		// 6桁のランダムなコードを生成 (100000から999999の範囲)
+		code = 100000 + rand.Intn(900000)
+
+		// 生成したコードが既に存在するかを確認
+		uuid, err := uc.uuidRepo.FindByCode(code)
+		if err != nil {
+			return nil, err
+		}
+		if uuid.Code != code {
+			break // 一意なコードが見つかったらループを抜ける
+		}
+	}
+
+	// UUIDを生成し、データベースに保存
 	generatedUUID := uc.uuidService.GenerateUUID(code)
 	uuid := &models.UUID{
 		UUID: generatedUUID,
