b35e782155d27588b68912e093fae9ab00490e4f
usamaru33 <raioraioraio15@gmail.com>
2024-11-05 17:59:56 +0900

    動きません...((´;ω;｀))

---
+++ dementia_quiz_VR/Assets/C#/Title/Get6Num.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using TMPro;
+using App.BaseSystem.DataStores.ScriptableObjects.Status;
+using System.Collections;
+
+public class SerialNumDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text displayText;
+
+    [SerializeField]
+    private StatusData statusData;
+
+    [Header("Update Settings")]
+    [SerializeField]
+    private float updateInterval = 2f; // çXêVä‘äuÅiïbÅj
+
+    private void Awake()
+    {
+        if (displayText == null || statusData == null)
+        {
+            Debug.LogError("Required components are not assigned!");
+            enabled = false;
+            return;
+        }
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(UpdateRoutine());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator UpdateRoutine()
+    {
+        while (true)
+        {
+            UpdateDisplay();
+            yield return new WaitForSeconds(updateInterval);
+        }
+    }
+
+    private void UpdateDisplay()
+    {
+        displayText.text = $"Serial Number: {statusData.SerialNum}";
+    }
+}
--- dementia_quiz_VR/Assets/C#/UserDB/StatusData.cs
+++ dementia_quiz_VR/Assets/C#/UserDB/StatusData.cs
@@ -3,38 +3,63 @@ using System.Collections.Generic;
 
 namespace App.BaseSystem.DataStores.ScriptableObjects.Status
 {
-    /// <summary>
-    /// �X�e�[�^�X�����I�u�W�F�N�g�̃f�[�^�Q (�Ώ�: �v���C���[�A�G�A�j��\�I�u�W�F�N�g�Ȃ�)
-    /// </summary>
     [CreateAssetMenu(menuName = "ScriptableObject/Data/Status")]
     public class StatusData : BaseData
     {
-        [SerializeField]
-        private List<int> quizDiff = new List<int>();
         public List<int> QuizDiff
         {
             get => quizDiff;
             set => quizDiff = value;
         }
         [SerializeField]
-        private int actDiff;
-        public int ActDiff
+        private List<int> quizDiff = new List<int>();
+
+        public float ActDiff
         {
             get => actDiff;
             set => actDiff = value;
         }
         [SerializeField]
+        private float actDiff;
+
+        public int SerialNum
+        {
+            get => serialNum;
+            set => serialNum = value;
+        }
+        [SerializeField]
+        private int serialNum;
+
         public string UUID
         {
             get => uuid;
             set => uuid = value;
         }
         [SerializeField]
-        private List<bool> lr = new List<bool>();
+        private string uuid;
+
         public List<bool> LR
         {
             get => lr;
             set => lr = value;
         }
+        [SerializeField]
+        private List<bool> lr = new List<bool>();
+
+        public float X
+        {
+            get => xValue;
+            set => xValue = value;
+        }
+        [SerializeField]
+        private float xValue;
+
+        public float Y
+        {
+            get => yValue;
+            set => yValue = value;
+        }
+        [SerializeField]
+        private float yValue;
     }
 }
